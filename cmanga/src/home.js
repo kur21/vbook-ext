d2f9273dc3a09afd5e2a400dc36bbb91b9b36cff
Hieu Nguyen <hieu21nt@gmail.com>
2022-12-03 22:37:00 +0700

    delete src, fix hentaivn

--- cmanga/src/home.js
+++
@@ -1,6 +0,0 @@
-function execute() {
-    return Response.success([
-        { title: "Cập Nhật", input: "new", script: "gen.js" },
-        { title: "Truyện Siêu Hay", input: "Truyện siêu hay", script: "source.js" },
-    ]);
-}
--- hentaivnvip/src/home.js
+++
@@ -1,8 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "Cập Nhật", input: "https://hentaivnvip.net/truyen-hentai-moi", script: "gen.js"},
-        {title: "Hot Tuần", input: "https://hentaivnvip.net/truyen-hot", script: "gen.js"},
-        {title: "Full Color", input: "https://hentaivnvip.net/the-loai/full-color", script: "gen.js"},
-        {title: "Không Che", input: "https://hentaivnvip.net/the-loai/khong-che", script: "gen.js"},
-    ]);
-}
