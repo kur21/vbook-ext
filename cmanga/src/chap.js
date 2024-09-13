ffa660ccd1ce7e377be0a16d99e0f786bc1e062d
Hieu Nguyen <hieu21nt@gmail.com>
2024-09-13 22:46:16 +0700

    update cmanga

--- cmanga/src/chap.js
+++ cmanga/src/chap.js
@@ -1,14 +1,16 @@
 load('config.js');
 function execute(url) {
     url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
-    // let time = Math.floor(new Date().getTime() / 1000).toString()
-    // Console.log(url + '&v=' + 1723040847)
-    // let response = fetch(url + '&v=' + time);
-    let response = fetch(url);
-    Console.log(url)
-    if (response.ok) {
-        let data = response.json();
-        return Response.success(data);
+    let res = fetch(BASE_URL + '/assets/inc/module//other/chapter_detail.php', {
+        method: 'POST'
+    })
+    if(res.ok) {
+        let response = fetch(url);
+        if (response.ok) {
+            let data = response.json();
+            return Response.success(data);
+        }
+        return null;
     }
     return null;
 }
--- cmanga/src/home.js
+++ cmanga/src/home.js
@@ -2,8 +2,10 @@ load('config.js');
 function execute() {
     return Response.success([
         { title: "Mới cập nhật", input: API + "/home_album_list?sort=update", script: "gen.js" },
-        { title: "Truyện Hot", input: API + "/home_album_list?sort=update&hot=on", script: "gen.js" },
-        { title: "Top view", input: API + "/home_album_list?sort=view", script: "gen.js" },
-        { title: "Top theo dõi", input: API + "/home_album_list?sort=follow", script: "gen.js" }
+        { title: "Hot", input: API + "/home_album_list?sort=update&type=hot", script: "gen.js" },
+        { title: "Mới", input: API + "/home_album_list?sort=update&type=new", script: "gen.js" },
+        { title: "Vip", input: API + "/home_album_list?sort=update&type=vip", script: "gen.js" },
+        { title: "Xu hướng", input: API + "/home_album_list?sort=update&type=trending", script: "gen.js" },
+        { title: "18+", input: API + "/home_album_list?sort=update&type=adult", script: "gen.js" },
     ]);
 }
