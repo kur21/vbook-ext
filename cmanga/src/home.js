18e695ba8811c3d3448753f90eed40eaa09053be
Hieu Nguyen <hieu21nt@gmail.com>
2024-09-13 22:51:49 +0700

    update home cmanga

--- cmanga/src/home.js
+++ cmanga/src/home.js
@@ -2,10 +2,9 @@ load('config.js');
 function execute() {
     return Response.success([
         { title: "Mới cập nhật", input: API + "/home_album_list?sort=update", script: "gen.js" },
-        { title: "Hot", input: API + "/home_album_list?sort=update&type=hot", script: "gen.js" },
-        { title: "Mới", input: API + "/home_album_list?sort=update&type=new", script: "gen.js" },
-        { title: "Vip", input: API + "/home_album_list?sort=update&type=vip", script: "gen.js" },
         { title: "Xu hướng", input: API + "/home_album_list?sort=update&type=trending", script: "gen.js" },
-        { title: "18+", input: API + "/home_album_list?sort=update&type=adult", script: "gen.js" },
+        { title: "Truyện Mới", input: API + "/home_album_list?sort=update&type=new", script: "gen.js" },
+        { title: "Truyện Vip", input: API + "/home_album_list?sort=update&type=vip", script: "gen.js" },
+        { title: "Truyện 18+", input: API + "/home_album_list?sort=update&type=adult", script: "gen.js" },
     ]);
 }
