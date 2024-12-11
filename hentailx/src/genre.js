3297bca03ab86e65dcc0fc196501d4541bcb533e
Hieu Nguyen <hieu21nt@gmail.com>
2024-12-11 09:12:10 +0700

    update category lxmanga

--- hentailx/src/genre.js
+++ hentailx/src/genre.js
@@ -2,7 +2,13 @@ load('src.js');
 function execute() {
     const doc = Http.get(BASE_URL).html();
     const el = doc.select(".mt-2 a");
-    const data = [];
+    const data = [
+        {
+            input: BASE_URL + "/the-loai/manhwa?sort=-updated_at&page=1&filter%5Bstatus%5D=1",
+            title: 'Manhwa đã hoàn thành',
+            script: 'cat.js'
+        }
+    ];
     for (var i = 0; i < el.size(); i++) {
         var e = el.get(i);
         data.push({
