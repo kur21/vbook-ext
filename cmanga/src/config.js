d015cc1280426918567d8e6574c7aca9741ea84e
Hieu Nguyen <hieu21nt@gmail.com>
2025-02-10 08:52:53 +0000

    update src

--- cmanga/src/config.js
+++ cmanga/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://cmangag.com";
+let BASE_URL = "https://cmangax.com";
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
--- hentailx/src/config.js
+++ hentailx/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL="https://lxmanga.info"
+let BASE_URL="https://lxmanga.cloud"
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
