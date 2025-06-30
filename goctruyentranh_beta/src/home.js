23fe3619b580e4a710dfe08628394c23ccaef779
Hieu Nguyen <hieu21nt@gmail.com>
2025-06-30 08:31:25 +0000

    remove beta + fix truyengg

--- goctruyentranh_beta/src/home.js
+++
@@ -1,7 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "Cập Nhật", input: "&orders%5B%5D=recentDate", script: "gen.js"},
-        {title: "Xem Nhiều", input: "&orders%5B%5D=viewCount", script: "gen.js"},
-        {title: "Hoàn Thành", input: "&orders%5B%5D=updatedAt&status%5B%5D=END", script: "gen.js"},
-    ]);
-}
